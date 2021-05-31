7734908644c8cab3387df4646ca4e808c631b5b1
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-05-31 23:32:39 +0200

    #10-Pequeño ajuste dto

--- ProyectoFinal.Core/DTO/UsuarioDto.cs
+++ ProyectoFinal.Core/DTO/UsuarioDto.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿
 
 namespace ProyectoFinal.Core.DTO
 {
-
-    public class UsuarioCreateRequest: AuthBaseRequest
+    public class UsuarioCreateRequest : AuthBaseRequest
     {
-        public string Login { get; set; }
-        public string ConfirmPassword { get; set; }
     }
 }
